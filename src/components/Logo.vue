3e43f0ee919244f53c269b128d5a5c79a0b91e08
Innovanito <khg05311@naver.com>
2021-09-25 15:13:32 +0900

    header done

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -6,8 +6,18 @@
 </template>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
+
 .logo {
   font-family: "Oswald", sans-serif;
-  
+  font-size: 20px;
+  color: $black;
+  text-decoration: none;
+  &:hover{
+    color: $black;
+  }
+  span{
+    color: $primary;
+  }
 }
 </style>
